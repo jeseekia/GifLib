0c94f46c7d11f260e368c05e32865d1b5a63c881
Jeseekia Vaughn <jeseekia@gmail.com>
2016-03-02 19:43:42 -0500

    Adding the ComponentScan annotation

--- src/main/java/com/metadevgirl/giflib/AppConfig.java
+++ src/main/java/com/metadevgirl/giflib/AppConfig.java
@@ -2,12 +2,14 @@ package com.metadevgirl.giflib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by Jeseekia on 3/2/16.
  */
 
 @EnableAutoConfiguration
+@ComponentScan
 public class AppConfig {
 
     public static void main (String[] args) {
